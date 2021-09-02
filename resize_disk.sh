7314c41eacf26c1f58eace146dcdb73d9621745c
Robert de Bock <robert@meinit.nl>
2021-09-02 11:23:51 +0200

    One function done.

---
+++ resize_disk.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+find_disks() {
+  # A function to find disks that are on LVM.
+  disks=$(mount | grep '^/dev/mapper' | awk '{ print $3 }')
+  length=$(echo ${disks} | wc -l)
+  if [ $length -gt 1 ] ; then
+    echo "What disk do you want to exend?"
+    echo
+    echo "${disks}"
+    return
+  else
+    echo "No LVM disks found that can be extended."
+    return 1
+  fi
+}
+
+find_volume_group() {
+  # A function to find a VG based on the mountpoint.
+  :
+}
+
+# scan_scsi
+#
+# check_vg_space
+# space: resize_lv
+# no_space: "Please resize the disk."
+#
+# resize_lv() {
+# find_free_space_on_vg
+# echo "How much space do you want to extend the disk with? (between x and y)"
+# lvextend ...
+#
+
+find_disks && read disk_to_extend
