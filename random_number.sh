97a09c1f42703d79174891433cc0fa1cc8198408
Robert de Bock <robert@meinit.nl>
2021-09-20 09:58:58 +0200

    Random.

---
+++ random_number.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# A script to return a random number, based on a least and most value.
+
+default_least="10"
+default_most="30"
+
+while getopts :l:m: flag
+do
+  case "${flag}" in
+    l)
+      least="${OPTARG}"
+    ;;
+    m)
+      most="${OPTARG}"
+    ;;
+    \?)
+      echo "The option ${OPTARG} is invalid."
+      exit 1
+    ;;
+    :)
+     echo "The argument ${OPTARG} is unknown."
+     exit 1
+    ;;
+  esac
+done
+
+if [ -z "${least}" ] ; then
+  least="${default_least}"
+fi
+
+if [ -z "${most}" ] ; then
+  most="${default_most}"
+fi
+
+if [ "${least}" -ge "${most}" ] ; then
+  echo "Least value (${least}) is higher or equal to the most value (${most})."
+  exit 1
+fi
+
+delta=$(($most - $least + 1))
+
+echo $(($least + $RANDOM % $delta))
