e6301cf435a1f52295f6a86ed78e5a9b1d68eec3
Robert de Bock <robert@meinit.nl>
2022-01-04 15:14:27 +0100

    named generator.

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - [resize_disk.sh](https://raw.githubusercontent.com/robertdebock/shell-scripts/main/resize_disk.sh) - A script to make Linux LVM disk resizing a little easier.
 - [random_number.sh](https://raw.githubusercontent.com/robertdebock/shell-scripts/main/random_number.sh) - Print a random number.
 - [vault_cleanup.sh](https://raw.githubusercontent.com/robertdebock/shell-scripts/main/vault_cleanup.sh) - Cleanup HashiCorp Vault leases.
+- [generate-named-conf.sh](https://raw.githubusercontent.com/robertdebock/shell-scripts/main/generate-named-conf.sh) - Generate Bind configuration.
---
+++ generate-named-conf.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# A script to generate named.conf. Quite specific.
+
+# Input: a file called `forwarders.txt`, with on each line:
+# - The zone.
+# - The nameserver(s) to forward to.
+#
+# For example:
+# example.com 1.2.3.4
+# 1.in-addr.arpa.net 2.3.4.5 3.4.5.6
+
+cat << EOF
+acl goodclients {
+  192.0.2.0/24;
+  localhost;
+  localnets;
+};
+
+options {
+  directory "/var/cache/bind";
+
+  recursion yes;
+  # TODO: change 'any' to 'goodclients' when done.
+  allow-query { any; };
+  allow-query-cache { any; };
+
+  forward only;
+  forwarders {
+    168.63.129.16;
+  };
+
+  dnssec-enable yes;
+  dnssec-validation yes;
+
+  auth-nxdomain no;    # conform to RFC1035
+  listen-on-v6 { any; };
+};
+
+EOF
+
+if [ -f forwarders.txt ] ; then
+  grep -v '^#' forwarders.txt | while read -r zone dnses ; do
+    echo "zone \"${zone}\" {"
+    echo "  type forward;"
+    echo "  forward only;"
+    printf "  forwarders { "
+    for dns in ${dnses} ; do
+      printf "%s; " "${dns}"
+    done
+    echo "};"
+    echo "};"
+    echo ""
+  done
+fi
